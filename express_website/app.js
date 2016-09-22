638bbb2f2628885bc2c723850b894d1d1ff0c7a4
Joe Coleman <phantom42@gmail.com>
2016-09-22 00:13:16 -0400

    completed lecture 9 - exprees routes and views

--- express_website/app.js
+++ express_website/app.js
@@ -5,13 +5,23 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
+// set up jade for view rendering
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
 	// console.log('hello world'); 
-	res.send('<h1>Hello world</h1>');
+	// res.send('<h1>Hello world</h1>');
+	res.render('index', {title: 'Welcome'});
+})
+
+app.get('/about', function(req, res){
+	res.render('about');
 })
 
 app.listen(3000);
