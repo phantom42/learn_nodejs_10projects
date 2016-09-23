9b0ce80f20ba95718a2925df47d70f2c5605d9d3
Joe Coleman <phantom42@gmail.com>
2016-09-22 23:45:18 -0400

    bug fixes. working on nodemailer bit.

--- express_website/app.js
+++ express_website/app.js
@@ -24,5 +24,36 @@ app.get('/about', function(req, res){
 	res.render('about');
 })
 
+app.get('/contact', function(req, res){
+	res.render('contact');
+})
+
+app.post('/contact/send', function(req, res){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'phantom42@gmail.com',
+			password: // enter password
+		}
+	});
+
+	var mailOptions = {
+		from: 'Joe <phantom42@gmail.com>',
+		to: 'jcoleman@phantom42.com',
+		subject: 'website submission',
+		text: 'you have a submission with the following details... Name: ' +req.body.name+ ' Email: ' + req.body.email + ' Message: ' + req.body.message,
+		html: '<p>you have a submision wiht the following details...</p><ul><li>' + req.body.name + '</li><li>' + req.body.email + '</li><li>' + req.body.message + '</li></ul>'
+	}
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('message sent: ' + info.response);
+			res.redirect('/');
+		}
+	})
+})
+
 app.listen(3000);
 console.log('server is running on port 3000');
